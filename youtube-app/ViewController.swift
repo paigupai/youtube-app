f5bba1b4e7f2672b54a5c450ec4a59db60353e21
paigupai <44311361+paigupai@users.noreply.github.com>
2020-06-11 01:03:40 +0900

    creat project

---
+++ youtube-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-app
+//
+//  Created by paigu on 2020/06/11.
+//  Copyright © 2020 paigu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
