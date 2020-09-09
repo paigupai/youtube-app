5c186a2fef567fd1486959043a81fd31537e6959
paigupai <44311361+paigupai@users.noreply.github.com>
2020-09-09 20:46:19 +0900

    Revert "Update Constants.swift"
    This reverts commit 4e7b155597d471dd56d0d4d0a9305afa814d69da.

--- youtube-app/Helpers/Constants.swift
+++ youtube-app/Helpers/Constants.swift
@@ -11,7 +11,7 @@ import Foundation
 struct Constants {
     //API_KEY 
     static var API_KEY = ""
-    static var PLAYLIST_ID = ""
+    static var PLAYLIST_ID = "UU2D6eRvCeMtcF5OGHf1-trw"
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     static var VIDEOCELL_ID = "VideoCell"
     
